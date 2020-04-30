3be28c65bb356e4b2041e5e43c0a95033c48dafa
Eino Lindberg <eino.lindberg@gmail.com>
2020-04-30 14:36:00 +0300

    Now runs with Linux as well.

--- libftprintf/includes/ft_printf.h
+++ libftprintf/includes/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elindber <elindber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elindber <elindber@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 14:00:48 by elindber          #+#    #+#             */
-/*   Updated: 2020/02/17 17:16:42 by elindber         ###   ########.fr       */
+/*   Updated: 2020/04/30 14:35:34 by elindber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdarg.h>
 # include <stdlib.h>
+# include <stdint.h>
 # include <unistd.h>
 # include <limits.h>
 # include <stdio.h>
